0515fc1265094b1f36ac3f105a0ae8f5ccf0fd54
Larissa Ferreira <larissadferr@gmail.com>
2025-12-18 15:07:13 -0500

    main.js - completed wireframe, main.css - start of css

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,50 @@
+// import governor selector
+// import facility selector
+// import render colony inventory
+// import render facility mineral selector
+// import render space cart
+
 const mainContainer = document.querySelector("#container")
 
 const ExomineHTML = () => {
+    // const governorSelectorHTML = GovernorSelector()
+    // const facilitySelectorHTML = FacilitySelector()
 
     return `
-    <header><h1>Solar System Mining MarketPlace</h1></header>
+    <header><h1>Solar System Mining Marketplace</h1></header>
     
-    <article id='GovFacSelector' class='selector-drop-down left'>
-        ***GOVERNOR SELECTOR***
-        ***FACILITY SELECTOR***
-    </article>
-
-    <article id='ColonyInventory' class='right'>
-        ***COLONY MINERALS***
+    <article id='GovFacSelector' class='selector-drop-down'>
+        <section class='selector-govfac left'>
+            <div class='selector--gov div-govfac'>
+                *** GOVERNOR SELECTOR ***
+            </div>
+            <div class='selector--fac div-govfac'>
+                *** FACILITY SELECTOR ***
+            </div>
+        </section>
+        <section class='inv-colony right'>
+            *** render COLONY MINERALS ***
+        </section>
     </article>
     
-    <article id='MinSelCart' class='mineral-selector space-cart bottom'>
-        ***MINERAL SELECTOR***
-        ***SPACE CART***
+    <article id='MinSelCart' class='bottom'>
+        <section class='selector-mineral left'>
+            <div class='selector--min'>
+                *** render MINERAL SELECTOR ***
+            </div>
+        </section>
+        <section class='space-cart right'>
+            <div class='cart'>
+                *** render SPACE CART ***
+            </div>
+        </section>
+        
     </article>
-
     `
 }
 
-
-const render = () => {
+const renderMain = () => {
     mainContainer.innerHTML = ExomineHTML()
 }
 
-render()
+renderMain()
