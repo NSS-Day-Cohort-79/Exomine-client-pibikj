459c4378f203846e6b2fc5c8153b66dc28027019
Larissa Ferreira <larissadferr@gmail.com>
2025-12-19 11:47:13 -0500

    ColonyInventory - complete renderColonyInventory function, updata ColonyManager to expand Colony

--- scripts/components/ColonyInventory.js
+++ scripts/components/ColonyInventory.js
@@ -1,9 +1,33 @@
-export const renderColonyInventory = () => {
+import { getColonyMineral } from "../managers/ColonyManager.js"
+import { getColonyId } from "../utilities/TransientState.js"
+
+export const renderColonyInventory = async () => {
     const colonyInventoryContainer = document.querySelector("#renderColonyMinerals")
 
     const colonyId = getColonyId()
 
     let colonyInventoryHTML = ""
     
-}
+    if (colonyId > 0) {
+        const colonyMinerals = await getColonyMineral(colonyId)
 
+        let colonyName = ""
+
+        const colonyMineralsMap = colonyMinerals.map((colonyMineral) => {
+            colonyName = colonyMineral.colony.name
+            return `<li>${colonyMineral.stock} tons of ${colonyMineral.mineral.name}</li>`
+        })
+
+        colonyInventoryHTML += `
+            <h2>${colonyName} Minerals</h2>
+            <ul>
+        `
+        colonyInventoryHTML += colonyMineralsMap.join("")
+        colonyInventoryHTML += `</ul>`
+
+    } else {
+        colonyInventoryHTML += `<h2>Colony Minerals</h2>`
+    }
+
+    colonyInventoryContainer.innerHTML = colonyInventoryHTML
+}
