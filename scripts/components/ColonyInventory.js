5de7118e28ac27c0802eba158da1bf1c80b9b0ad
Larissa Ferreira <larissadferr@gmail.com>
2025-12-19 13:09:23 -0500

    ColonyInventory - updated getting colony name, and colony name display

--- scripts/components/ColonyInventory.js
+++ scripts/components/ColonyInventory.js
@@ -1,29 +1,31 @@
-import { getColonyMineral } from "../managers/ColonyManager.js"
-import { getColonyId } from "../utilities/TransientState.js"
+import { getColonies, getColonyMineral } from "../managers/ColonyManager.js"
+import { getColonyId, setColonyId } from "../utilities/TransientState.js"
 
 export const renderColonyInventory = async () => {
     const colonyInventoryContainer = document.querySelector("#renderColonyMinerals")
-
     const colonyId = getColonyId()
 
     let colonyInventoryHTML = ""
     
     if (colonyId > 0) {
         const colonyMinerals = await getColonyMineral(colonyId)
+        const colonies = await getColonies()
+        const colonyName = colonies.find((colony) => (colonyId === colony.id)).name
 
-        let colonyName = ""
+        if (colonyMinerals.length == 0) {
+            colonyInventoryHTML += `<h2 class="colony-mineral-header colony-min-empty">${colonyName} Minerals</h2>`
+        } else {
+            const colonyMineralsMap = colonyMinerals.map((colonyMineral) => {
+                return `<li data-stock="${colonyMineral.stock}" data-mineral="${colonyMineral.mineral.name}" data-colonyname="${colonyMineral.colony.name}">${colonyMineral.stock} tons of ${colonyMineral.mineral.name}</li>`
+            })
 
-        const colonyMineralsMap = colonyMinerals.map((colonyMineral) => {
-            colonyName = colonyMineral.colony.name
-            return `<li data-stock="${colonyMineral.stock}" data-mineral="${colonyMineral.mineral.name}" data-colonyname="${colonyMineral.colony.name}">${colonyMineral.stock} tons of ${colonyMineral.mineral.name}</li>`
-        })
-
-        colonyInventoryHTML += `
-            <h2 class="colony-mineral-header">${colonyName} Minerals</h2>
-            <ul class="colony-minerals-list">
-        `
-        colonyInventoryHTML += colonyMineralsMap.join("")
-        colonyInventoryHTML += `</ul>`
+            colonyInventoryHTML += `
+                <h2 class="colony-mineral-header">${colonyName} Minerals</h2>
+                <ul class="colony-minerals-list">
+            `
+            colonyInventoryHTML += colonyMineralsMap.join("")
+            colonyInventoryHTML += `</ul>`
+        }
 
     } else {
         colonyInventoryHTML += `<h2>Colony Minerals</h2>`
