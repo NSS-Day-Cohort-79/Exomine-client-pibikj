23f44b06bd78bfed612cab989f21c0feac31b8df
cmdrumright <cdrumright@protonmail.com>
2026-01-01 21:10:06 -0600

    updated FacilityMineralManager to handle multiple mineral selections - FacilityMineralManager 	- changed radio buttons to regular buttons 	- added check if cart qty reached facility stock to disable button

--- scripts/components/FacilityMineralSelector.js
+++ scripts/components/FacilityMineralSelector.js
@@ -1,24 +1,27 @@
 import { getFacilityMinerals } from "../managers/FacilityManager.js"
-import { getFacilityId, addToCart } from "../utilities/TransientState.js"
+import { getFacilityId, addToCart, getCart } from "../utilities/TransientState.js"
 import { renderSpaceCart } from "./SpaceCart.js"
 
 
-const handleFacilityMineralOption = (changeEvent) => {
-    const itemChanged = changeEvent.target
-    if (itemChanged.dataset.facilityactive === "true") {
+const handleFacilityMineralOption = (event) => {
+    const itemChanged = event.target
+    if (itemChanged.className === "mineral-button" && !itemChanged.disabled) {
         addToCart(parseInt(itemChanged.dataset.mineralid))
         renderSpaceCart()
+        renderMineralSelector()
     }
 }
 
 
 export const renderMineralSelector = async () => {
-    document.addEventListener("change", handleFacilityMineralOption)
+    document.addEventListener("click", handleFacilityMineralOption)
     const mineralListId = document.querySelector("#renderFacilityMineralSelector")
     const facilityId = getFacilityId()
     let html = "<h2>Facility Minerals</h2>"
 
     if (facilityId > 0) {
+        const cart = getCart()
+        const facilityCart = cart.filter((item) => item.facilityId === facilityId)
         const facilityMinerals = await getFacilityMinerals(facilityId)
         const facilityMineralArray = facilityMinerals.map(
             (facilityMineral) => {
@@ -26,8 +29,23 @@ export const renderMineralSelector = async () => {
                     return ""
                 } else {
                     let disabled = ""
+                    let cartItem = facilityCart.find((item) => item.mineralId === facilityMineral.mineralId)
+                    if (cartItem) {
+                        if (cartItem.qty >= facilityMineral.stock) {
+                            disabled = "disabled"
+                        }
+                    }
                     if (!facilityMineral.facility.isActive) {disabled = "disabled"}
-                    return `<input ${disabled} type="radio" name="facilitymineral" data-mineralid="${facilityMineral.mineralId}" data-facilityActive="${facilityMineral.facility.isActive}" data-facilityName="${facilityMineral.facility.name}" data-mineralName="${facilityMineral.mineral.name}" id="${facilityMineral.id}" /> ${facilityMineral.stock} tons of ${facilityMineral.mineral.name}`
+                    return `
+                        <button ${disabled}
+                            class="mineral-button"
+                            id="add-mineral-${facilityMineral.id}" 
+                            data-mineralid="${facilityMineral.mineralId}">
+                                add to cart
+                        </button>
+                        ${facilityMineral.stock} tons of ${facilityMineral.mineral.name}
+                        <br>
+                    `
                 }
             }
         )
